76c89afe57fe3aed0b01ec27bd2be2845b3f5de2
kumbamsandeep <kumbamsandeep123@gmail.com>
2017-05-27 23:23:05 +0530

    Db code.

--- app/src/main/java/com/example/nanni/myapplication/PreviousOrdersActivity.java
+++ app/src/main/java/com/example/nanni/myapplication/PreviousOrdersActivity.java
@@ -18,7 +18,7 @@ public class PreviousOrdersActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_previousOrders);
+        setContentView(R.layout.activity_previous_orders);
         mPreviousOrderList=(ListView)findViewById(R.id.lv_previousOrders);
 
     }
